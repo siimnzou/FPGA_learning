7db8cfd4e3c9fab74efd97c3a671338b7d706279
siimnzou <95514915+siimnzou@users.noreply.github.com>
2022-04-11 18:51:31 +0800

    添加SDRAM串口模块quartus项目文件

---
+++ Library/SDRAM/SDRAM_UART/quartusprj/ip_core/sdram_fifo/sdram_fifo_inst.v
@@ -0,0 +1,11 @@
+sdram_fifo	sdram_fifo_inst (
+	.aclr ( aclr_sig ),
+	.data ( data_sig ),
+	.rdclk ( rdclk_sig ),
+	.rdreq ( rdreq_sig ),
+	.wrclk ( wrclk_sig ),
+	.wrreq ( wrreq_sig ),
+	.q ( q_sig ),
+	.rdusedw ( rdusedw_sig ),
+	.wrusedw ( wrusedw_sig )
+	);
